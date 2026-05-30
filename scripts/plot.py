27cd6b7a6a87f376dc1bce8e17b581c1596956ae
matchingHui <1123085891@qq.com>
2026-05-30 10:55:45 +0800

    新增mma_fp16acc_m16n32_staged_cpasync_k64_4x2，相对 K32 cp.async 基本全尺寸提升。下一步做skew16

--- scripts/plot.py
+++ scripts/plot.py
@@ -89,6 +89,7 @@ PHASE2_4090_IMPLS = [
     "mma_fp16acc_m16n32_k32_4x2",
     "mma_fp16acc_m16n32_k32_4x4",
     "mma_fp16acc_m16n32_staged_cpasync_k32_4x2",
+    "mma_fp16acc_m16n32_staged_cpasync_k64_4x2",
     # "cublas_gemmex_fp16acc",
     "cublaslt_fp16acc",
 ]
