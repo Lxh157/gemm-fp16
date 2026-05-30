aabc61218b16d8bdd6a5f6f8e2e9a3a5ba68023c
matchingHui <1123085891@qq.com>
2026-05-30 12:15:44 +0800

    新增mma_fp16acc_m16n32_staged_cpasync_k64_4x2_skew32，明显不如skew16，根据wmma的多padding对比经验，shew16为padding最优参数。下一步考虑warp4x1

--- scripts/plot.py
+++ scripts/plot.py
@@ -91,6 +91,7 @@ PHASE2_4090_IMPLS = [
     "mma_fp16acc_m16n32_staged_cpasync_k32_4x2",
     "mma_fp16acc_m16n32_staged_cpasync_k64_4x2",
     "mma_fp16acc_m16n32_staged_cpasync_k64_4x2_skew16",
+    "mma_fp16acc_m16n32_staged_cpasync_k64_4x2_skew32",
     "mma_fp16acc_m16n32_staged_cpasync_k64_4x4_skew16",
     # "cublas_gemmex_fp16acc",
     "cublaslt_fp16acc",
